b352189ac4e0f968d4a3be35094f0c21a39a5618
Fabricio Izumi <fabricio.bandeira@tpv-tech.com>
2024-07-10 19:21:51 -0400

    Included new file to keep custom functions

---
+++ custom.el
@@ -0,0 +1,11 @@
+(defun my-first-function ()
+       "Primeira funcao."
+       (interactive)
+       ;;; Code:
+       (message "Hello World de novo e novo!!")
+       ;;body function
+)
+
+(provide 'custom-module)
+;;; custom.el ends here
+
